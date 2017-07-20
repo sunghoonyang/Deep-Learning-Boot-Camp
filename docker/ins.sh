f31baf590dc2a9b81d56aaaca44a8a4c8051c1d5
Solomon K <shlomokashani@gmail.com>
2017-07-20 19:26:26 +0300

    Add files via upload

---
+++ docker/ins.sh
@@ -0,0 +1,66 @@
+apt-get update && apt-get install -y software-properties-common && \
+    apt-get install -y --no-install-recommends \
+        build-essential \
+        clinfo \
+        cmake \
+        git \
+        libboost-all-dev \
+        libfftw3-dev \
+        libfontconfig1-dev \
+        libfreeimage-dev \
+        liblapack-dev \
+        liblapacke-dev \
+        libopenblas-dev \
+        ocl-icd-opencl-dev \
+        opencl-headers \
+        wget \
+        xorg-dev && \
+    rm -rf /var/lib/apt/lists/*
+
+
+ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/lib/libcuda.so.1 && \
+    ln -s /usr/lib/libcuda.so.1 /usr/lib/libcuda.so && \
+    mkdir -p /etc/OpenCL/vendors && \
+    echo "libnvidia-opencl.so.1" > /etc/OpenCL/vendors/nvidia.icd && \
+    echo "/usr/local/nvidia/lib" >> /etc/ld.so.conf.d/nvidia.conf && \
+    echo "/usr/local/nvidia/lib64" >> /etc/ld.so.conf.d/nvidia.conf
+export  PATH=/usr/local/nvidia/bin:/usr/local/cuda/bin:${PATH}
+
+
+# Build GLFW from source
+git clone https://github.com/glfw/glfw.git && \
+    cd glfw && \
+    mkdir build && \
+    cd build && \
+    cmake -DCMAKE_INSTALL_PREFIX=/usr .. && \
+    make -j4 && \
+    make install
+
+
+# AF_DISABLE_GRAPHICS - Environment variable to disable graphics at
+# runtime due to lack of graphics support by docker - visit
+# http://arrayfire.org/docs/configuring_environment.htm#af_disable_graphics
+# for more information
+export AF_PATH=/opt/arrayfire AF_DISABLE_GRAPHICS=1
+#ARG COMPILE_GRAPHICS=OFF
+git clone --recursive https://github.com/arrayfire/arrayfire.git -b master && \
+    cd arrayfire && mkdir build && cd build && \
+    cmake .. -DCMAKE_INSTALL_PREFIX=/opt/arrayfire-3 \
+             -DCMAKE_BUILD_TYPE=Release \
+             -DBUILD_CPU=ON \
+             -DBUILD_CUDA=ON \
+             -DBUILD_OPENCL=ON \
+             -DBUILD_UNIFIED=ON \
+             -DBUILD_GRAPHICS=OFF \
+             -DBUILD_NONFREE=OFF \
+             -DBUILD_EXAMPLES=ON \
+             -DBUILD_TEST=ON \
+             -DBUILD_DOCS=OFF \
+             -DINSTALL_FORGE_DEV=ON \
+             -DUSE_FREEIMAGE_STATIC=OFF && \
+             # -DCOMPUTES_DETECTED_LIST="30;35;37;50;52;60" \
+    make -j8 && make install && \
+    mkdir -p ${AF_PATH} && ln -s /opt/arrayfire-3/* ${AF_PATH}/ && \
+    echo "${AF_PATH}/lib" >> /etc/ld.so.conf.d/arrayfire.conf && \
+    echo "/usr/local/cuda/nvvm/lib64" >> /etc/ld.so.conf.d/arrayfire.conf && \
+    ldconfig
