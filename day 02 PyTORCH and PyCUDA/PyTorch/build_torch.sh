334ed2731762a3fce5bafe6e19c8d544fee28ff8
quantscientist <shlomokashani@gmail.com>
2017-09-05 20:19:57 +0300

    conv1d

--- day 02 PyTORCH and PyCUDA/PyTorch/build_torch.sh
+++ day 02 PyTORCH and PyCUDA/PyTorch/build_torch.sh
@@ -10,22 +10,30 @@ export PATH=/usr/local/nvidia/bin:/usr/local/cuda/bin:${PATH}
 export CUDA_BIN_PATH=/usr/local/cuda
 export CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0
 
+
 # Build PyTorch from source
-git clone https://github.com/pytorch/pytorch.git
+#git clone https://github.com/pytorch/pytorch.git
 cd pytorch
-git checkout 4eb448a051a1421de1dda9bd2ddfb34396eb7287 
+git submodule update --init
+#git checkout 4eb448a051a1421de1dda9bd2ddfb34396eb7287 
 
 export TORCH_CUDA_ARCH_LIST="3.5 5.2 6.0 6.1+PTX" 
 export TORCH_NVCC_FLAGS="-Xfatbin -compress-all" 
+
+#pip uninstall torch
+#python setup.py clean
+#python setup.py build
 python setup.py install
+
 
 # Build torch-vision from source
 git clone https://github.com/pytorch/vision.git 
 cd vision
-git checkout 83263d8571c9cdd46f250a7986a5219ed29d19a1 
+#git checkout 83263d8571c9cdd46f250a7986a5219ed29d19a1 
+git submodule update --init
 python setup.py install 
 
 
 # CPU version
-pip install git+https://github.com/pytorch/tnt.git@master
+#pip install git+https://github.com/pytorch/tnt.git@master
 
