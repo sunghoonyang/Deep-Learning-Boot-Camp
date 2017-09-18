1f8fc29f9063b07d88ad46d6a99a1e68862e505d
Solomon K <shlomokashani@gmail.com>
2017-09-18 16:08:21 +0300

    Update ins.sh

--- docker/ins.sh
+++ docker/ins.sh
@@ -218,7 +218,7 @@ export CUDA_BIN_PATH=/usr/local/cuda
 export CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0
 
 # torch
-pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl
+#pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl
 pip install https://s3.amazonaws.com/pytorch/whl/cu75/torch-0.1.6.post22-cp27-none-linux_x86_64.whl 
 pip install torchvision
  
