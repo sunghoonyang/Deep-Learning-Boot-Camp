57b7eb49c049965a3adab77ec84f5ff6a96745f9
quantscientist <shlomokashani@gmail.com>
2017-12-07 15:01:36 +0200

    SeNet wron parameters

--- Kaggle-PyTorch/PyTorch-Ensembler/nnmodels/senet.py
+++ Kaggle-PyTorch/PyTorch-Ensembler/nnmodels/senet.py
@@ -3,142 +3,15 @@ import math
 import torch.nn as nn
 from torchvision.models import ResNet
 
-__all__ = ['sesnet', 'IceSEBasicBlock', 'senetXX_generic']
+__all__ = ['sesnet', 'IceSEBasicBlock', 'senetXX_generic', 'senet32_RG_1_classes']
 
 
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=True)
 
 
-class SEBasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, reduction=16):
-        super(SEBasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes, 1)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.se = SELayer(planes, reduction)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.se(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-class SEBottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, reduction=16):
-        super(SEBottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 4)
-        self.relu = nn.ReLU(inplace=True)
-        self.se = SELayer(planes * 4, reduction)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-        out = self.se(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-def se_resnet18(num_classes):
-    """Constructs a ResNet-18 model.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(SEBasicBlock, [2, 2, 2, 2], num_classes=num_classes)
-    model.avgpool = nn.AdaptiveAvgPool2d(1)
-    return model
-
-
-def se_resnet34(num_classes):
-    """Constructs a ResNet-34 model.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(SEBasicBlock, [3, 4, 6, 3], num_classes=num_classes)
-    model.avgpool = nn.AdaptiveAvgPool2d(1)
-    return model
-
-
-def se_resnet50(num_classes):
-    """Constructs a ResNet-50 model.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(SEBottleneck, [3, 4, 6, 3], num_classes=num_classes)
-    model.avgpool = nn.AdaptiveAvgPool2d(1)
-    return model
-
-
-def se_resnet101(num_classes):
-    """Constructs a ResNet-101 model.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(SEBottleneck, [3, 4, 23, 3], num_classes=num_classes)
-    model.avgpool = nn.AdaptiveAvgPool2d(1)
-    return model
-
-
-def se_resnet152(num_classes):
-    """Constructs a ResNet-152 model.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(SEBottleneck, [3, 8, 36, 3], num_classes=num_classes)
-    model.avgpool = nn.AdaptiveAvgPool2d(1)
-    return model
-
-
-REDUCTION = 8
-
-
 class SELayer(nn.Module):
-    def __init__(self, channel, reduction=REDUCTION):
+    def __init__(self, channel, reduction=16):
         super(SELayer, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Sequential(
@@ -159,7 +32,7 @@ class SELayer(nn.Module):
 class IceSEBasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, reduction=REDUCTION):
+    def __init__(self, inplanes, planes, reduction=16):
         super(IceSEBasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -201,7 +74,6 @@ class IceResNet(nn.Module):
         self.layer1 = self._make_layer(block, self.inplane, blocks=2 * n_size, stride=2)
         self.layer2 = self._make_layer(block, self.inplane * 2, blocks=2 * n_size, stride=2)
         self.layer3 = self._make_layer(block, self.inplane * 4, blocks=2 * n_size, stride=2)
-        self.layer4 = self._make_layer(block, self.inplane * 4, blocks=2 * n_size, stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d(1)
 
         self.fc = nn.Linear(int(8 * self.base), num_classes)
@@ -233,7 +105,6 @@ class IceResNet(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        # x = self.layer4(x)
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
@@ -245,13 +116,20 @@ class IceResNet(nn.Module):
         return x
 
 
-def senetXX_generic(num_classes, num_rgb, base):
-    model = IceResNet(IceSEBasicBlock, 1, num_classes, num_rgb, base)  # 56
+def senet16_RGB_10_classes(num_classes=10, num_rgb=3):
+    model = IceResNet(IceSEBasicBlock, 1, num_classes, num_rgb, 16)  # 56
     return model
 
 
-def sesnet(**kwargs):
-    """
-    Constructs a ResNet model.
-    """
-    return ResNet(**kwargs)
+def senet16_RG_1_classes(num_classes=1, num_rgb=2):
+    model = IceResNet(IceSEBasicBlock, 1, num_classes, num_rgb, 16)  # 56
+    return model
+
+
+def senet32_RG_1_classes(num_classes=1, num_rgb=2):
+    model = IceResNet(IceSEBasicBlock, 1, num_classes, num_rgb, 32)  # 56
+    return model
+
+def senetXX_generic(num_classes, num_rgb, base):
+    model = IceResNet(IceSEBasicBlock, 1, num_classes, num_rgb, base)  # 56
+    return model
