add6f5ce1d38a31f32430964b9c941be77b16dff
sunghoonyang <sunghoonyang@berkeley.edu>
2019-05-02 01:33:49 -0400

    CNN conv1d for Numerai

--- Kaggle-PyTorch/PyTorch-Ensembler/main-dl19.py
+++ Kaggle-PyTorch/PyTorch-Ensembler/main-dl19.py
@@ -185,7 +185,20 @@ def loadDB(args):
 
     train_data = df.sample(frac=args.validationRatio)
     valid_data = df[~df['file'].isin(train_data['file'])]
-
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    }
     train_set = SeedDataset(train_data, args.data_path, transform=data_transforms['train'])
     valid_set = SeedDataset(valid_data, args.data_path, transform=data_transforms['val'])
 
